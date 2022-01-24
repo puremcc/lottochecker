ba605f1bbe6076883c29ae51b7cd3507111d64d7
Mike Duffy <mike@github.mduffy.me>
2022-01-23 22:57:00 -0600

    Feature: ETL fix and SNS for ETL job failure (#18)
    * Added: SNS for ETL job failure
    
    * Fixed: Issue with ETL job summary return obj from last PR
    
    * Updated arch diagram

--- backend/functions/etl/app.py
+++ backend/functions/etl/app.py
@@ -1,5 +1,4 @@
 import json
-from logging import raiseExceptions
 
 import loader
 from loader import Games
@@ -8,10 +7,10 @@ from loader import Games
 def lambda_handler(event, context):
     response = {'statusCode': 200}
 
-    job_summary = {}.update(
-        loader.run_etl(Games.LOTTO_TEXAS),
-        loader.run_etl(Games.MEGAMILLIONS)
-    )
+    job_summary = {
+        **loader.run_etl(Games.LOTTO_TEXAS),
+        **loader.run_etl(Games.MEGAMILLIONS)
+    }
     response['body'] = json.dumps(job_summary)
     print(response['body'])
 
